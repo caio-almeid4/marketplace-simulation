844830c67dc166b6adec992c76e90fbe18cd4811
Caio Almeida <mcalm.139@gmail.com>
2025-12-29 22:22:12 -0300

    feat: added an inventory schema

---
+++ schemas/inventory.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+
+
+class Inventory(BaseModel):
+
+    cash: float = Field(description="Agent's total amount of money")
+    apple: int = Field(description="Agent's total amount of apples")
+    chip: int = Field(description="Agent's total amount of chips")
+    gold: int = Field(description="Agent's Total amount of money")
