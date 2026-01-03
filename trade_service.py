ccc9838319fdbd7a8b6888bee402386a0c95ac8c
Caio Almeida <mcalm.139@gmail.com>
2026-01-03 18:41:22 -0300

    chore: change function name

--- trade_service.py
+++ trade_service.py
@@ -8,7 +8,7 @@ class TradeService:
     def __init__(self, session: Session):
         self.session = session
 
-    def create_trade_registry(self, buyer_name: str, offer: TrackedOffer) -> Trade:
+    def create_trade_db_registry(self, buyer_name: str, offer: TrackedOffer) -> Trade:
         trade = Trade(**offer.model_dump(), buyer=buyer_name)
 
         self.session.add(trade)
